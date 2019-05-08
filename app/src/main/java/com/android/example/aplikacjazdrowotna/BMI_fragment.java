52cb089777407eebb8fb6d3bc2ca0d37b752ce9d
Julia <julia2725@wp.pl>
2019-05-08 19:19:23 +0200

    Code added to the calculate_WHR method & text_description field added to the whr_fragment.xml

--- app/src/main/java/com/android/example/aplikacjazdrowotna/BMI_fragment.java
+++ app/src/main/java/com/android/example/aplikacjazdrowotna/BMI_fragment.java
@@ -3,8 +3,10 @@ package com.android.example.aplikacjazdrowotna;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -24,7 +26,7 @@ public class BMI_fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.bmi_fragment, container, false);
         Toolbar toolbar = getActivity().findViewById(R.id.toolbar);
-        ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
+        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
 
         text_bmi_weight = view.findViewById(R.id.text_weight_bmi);
         text_bmi_height = view.findViewById(R.id.text_height_bmi);
@@ -34,48 +36,51 @@ public class BMI_fragment extends Fragment {
         edit_bmi_weight = view.findViewById(R.id.edit_weight_bmi);
         button_calculate_bmi = view.findViewById(R.id.button_calculate_bmi);
         button_calculate_bmi.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                calculateBMI();
-            }
-        }
+                                                    @Override
+                                                    public void onClick(View view) {
+                                                        calculateBMI();
+                                                    }
+                                                }
         );
 
         return view;
     }
 
     public void calculateBMI() {
-        float height = Float.parseFloat(edit_bmi_height.getText().toString()) / 100;
-        float weight = Float.parseFloat(edit_bmi_weight.getText().toString());
-        float BMI = weight / (height * height);
-        BMI = Math.round(BMI);
+            Float height = Float.parseFloat(edit_bmi_height.getText().toString()) / 100;
+            Float weight = Float.parseFloat(edit_bmi_weight.getText().toString());
+            Float BMI = weight / (height * height);
+
         String description;
 
-        if (BMI < 16) {
-            description = "Twoje BMI wynosi: " + BMI + "."+ System.lineSeparator()+ "Oznacza to wygłodzenie.";
-            text_show_bmi.setText(description);
-        } else if (BMI >= 16 && BMI <= 16.99) {
-            description = "Twoje BMI wynosi: " + BMI + "." + System.lineSeparator() +"Oznacza to wychudzenie.";
-            text_show_bmi.setText(description);
-        } else if (BMI >= 17 && BMI <= 18.49) {
-            description = "Twoje BMI wynosi: " + BMI +"."+ System.lineSeparator()+ "Oznacza to niedowagę.";
-            text_show_bmi.setText(description);
-        } else if (BMI >= 18.50 && BMI <= 24.99) {
-            description = "Twoje BMI wynosi: " + BMI + "."+System.lineSeparator() + "Oznacza to prawidłową wagę.";
-            text_show_bmi.setText(description);
-        } else if (BMI >= 25 && BMI <= 29.99) {
-            description = "Twoje BMI wynosi: " + BMI + "."+System.lineSeparator() + "Oznacza to nadwagę.";
-            text_show_bmi.setText(description);
-        } else if (BMI >= 30 && BMI <= 34.99) {
-            description = "Twoje BMI wynosi: " + BMI +"."+ System.lineSeparator()+ "Oznacza to otyłość I stopnia.";
-            text_show_bmi.setText(description);
-        } else if (BMI >= 35 && BMI <= 39.99) {
-            description = "Twoje BMI wynosi: " + BMI + "."+System.lineSeparator() + "Oznacza to otyłość II stopnia.";
-            text_show_bmi.setText(description);
-        } else if (BMI >= 40) {
-            description = "Twoje BMI wynosi: " + BMI +"."+ System.lineSeparator() +"Oznacza to skrajną otyłość.";
-            text_show_bmi.setText(description);
+        if (height != null && weight != null) {
+            if (BMI < 16) {
+                description = "Twoje BMI wynosi: " + BMI + "." + System.lineSeparator() + "Oznacza to wygłodzenie.";
+                text_show_bmi.setText(description);
+            } else if (BMI >= 16 && BMI <= 16.99) {
+                description = "Twoje BMI wynosi: " + BMI + "." + System.lineSeparator() + "Oznacza to wychudzenie.";
+                text_show_bmi.setText(description);
+            } else if (BMI >= 17 && BMI <= 18.49) {
+                description = "Twoje BMI wynosi: " + BMI + "." + System.lineSeparator() + "Oznacza to niedowagę.";
+                text_show_bmi.setText(description);
+            } else if (BMI >= 18.50 && BMI <= 24.99) {
+                description = "Twoje BMI wynosi: " + BMI + "." + System.lineSeparator() + "Oznacza to prawidłową wagę.";
+                text_show_bmi.setText(description);
+            } else if (BMI >= 25 && BMI <= 29.99) {
+                description = "Twoje BMI wynosi: " + BMI + "." + System.lineSeparator() + "Oznacza to nadwagę.";
+                text_show_bmi.setText(description);
+            } else if (BMI >= 30 && BMI <= 34.99) {
+                description = "Twoje BMI wynosi: " + BMI + "." + System.lineSeparator() + "Oznacza to otyłość I stopnia.";
+                text_show_bmi.setText(description);
+            } else if (BMI >= 35 && BMI <= 39.99) {
+                description = "Twoje BMI wynosi: " + BMI + "." + System.lineSeparator() + "Oznacza to otyłość II stopnia.";
+                text_show_bmi.setText(description);
+            } else if (BMI >= 40) {
+                description = "Twoje BMI wynosi: " + BMI + "." + System.lineSeparator() + "Oznacza to skrajną otyłość.";
+                text_show_bmi.setText(description);
+            }
         }
+
 
     }
 }
--- app/src/main/java/com/android/example/aplikacjazdrowotna/WHR_fragment.java
+++ app/src/main/java/com/android/example/aplikacjazdrowotna/WHR_fragment.java
@@ -14,9 +14,11 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
+import static android.text.Layout.JUSTIFICATION_MODE_INTER_WORD;
+
 public class WHR_fragment extends Fragment {
 
-    TextView text_whr_waist, text_whr_hip, text_your_WHR, text_show_whr, text_whr_sex;
+    TextView text_whr_waist, text_whr_hip, text_your_WHR, text_show_whr, text_whr_sex, text_description;
     EditText edit_whr_waist, edit_whr_hip;
     Button button_calculate_whr;
     RadioButton radio_whr_woman, radio_whr_man;
@@ -27,13 +29,14 @@ public class WHR_fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.whr_fragment, container, false);
         Toolbar toolbar = getActivity().findViewById(R.id.toolbar);
-        ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
+        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
 
         text_whr_waist = view.findViewById(R.id.text_waist_whr);
         text_whr_hip = view.findViewById(R.id.text_hip_whr);
         text_your_WHR = view.findViewById(R.id.text_your_WHR);
         text_show_whr = view.findViewById(R.id.text_show_whr);
         text_whr_sex = view.findViewById(R.id.text_whr_sex);
+        text_description = view.findViewById(R.id.text_description);
         edit_whr_hip = view.findViewById(R.id.edit_hip_whr);
         edit_whr_waist = view.findViewById(R.id.edit_waist_whr);
         button_calculate_whr = view.findViewById(R.id.button_calculate_whr);
@@ -42,44 +45,35 @@ public class WHR_fragment extends Fragment {
         radioGroup_whr = view.findViewById(R.id.radioGroup_whr);
 
         button_calculate_whr.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                calculate_WHR();
-            }
-        }
+                                                    @Override
+                                                    public void onClick(View view) {
+                                                        calculate_WHR();
+                                                    }
+                                                }
         );
 
         return view;
     }
 
     public void calculate_WHR() {
-        float hip = Float.parseFloat(edit_whr_hip.getText().toString());
-        float waist = Float.parseFloat(edit_whr_waist.getText().toString());
+        Float hip = Float.parseFloat(edit_whr_hip.getText().toString());
+        Float waist = Float.parseFloat(edit_whr_waist.getText().toString());
 
-
-        float WHR = waist /hip;
-        WHR = Math.round(WHR);
+        Float WHR = waist / hip;
         String description;
 
-
-
-
+        if ((radio_whr_woman.isChecked() && WHR >= 0.8) || (!radio_whr_man.isChecked() && WHR >= 1.0)) {
+            description = "Twój wskaźnik WHR wynosi: " + WHR + "." + System.lineSeparator() + "Wskazuje " +
+                    "to na otyłość typu jabłko. ";
+            text_show_whr.setText(description);
+        } else if ((radio_whr_woman.isChecked() && WHR < 0.8) || (radio_whr_man.isChecked() && WHR < 1.0)) {
+            description = "Twój wskaźnik WHR wynosi: " + WHR + "." + System.lineSeparator() + "Wskazuje " +
+                    "to na otyłość typu gruszka. ";
+            text_show_whr.setText(description);
         }
-
-        public String checkButton(View v) {
-            int radioId = radioGroup_whr.getCheckedRadioButtonId();
-  //          RadioButton radioButton = (RadioButton)findViewById(radioId);
-            String sex = "";
-
-     //       if (radioButton == radio_whr_man) {
-     //           sex="M";
-      //      } else if (radioButton == radio_whr_woman) {
-      //          sex="K";
-      //      }
-            return sex;
-        }
-
-
     }
+
+
+}
 
 
